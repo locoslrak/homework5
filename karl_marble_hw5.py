1ed4cd30f992794f0816111cf5c067e28cac34b8
Karl <karlmarble@mail.weber.edu>
2016-10-27 18:50:13 +0000

    Basic function skeleton

--- karl_marble_hw5.py
+++ karl_marble_hw5.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import sys
 
-
-
+def valid(pin):
+    """
+    Takes a 4 digit input and checks it against a valid code.
+    """
+    pass
 
 
 def main():
+    """
+    Test Function
+    """
     pass
 
 if __name__ == '__main__':
