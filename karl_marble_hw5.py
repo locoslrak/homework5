e73d78ab88c45063ce7ed7b502d9c00760a568a1
Karl <karlmarble@mail.weber.edu>
2016-10-28 19:40:38 +0000

    Completed tasks 1-4

--- karl_marble_hw5.py
+++ karl_marble_hw5.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 import sys
 
-def valid(pin):
+def valid():
     """
     Takes a 4 digit input and checks it against a valid code.
     Args:
-        pin - a 4 digit passcode
+        None
+    Returns:
+        None
     """
-    #1) Check the length
+    #1) Take the Pin Input
+    #2) Check the length
     #2) Check the type
     #3) Check against the code
-    len(pin) = strLength
+    pin = input("Enter your PIN:")
+    strLength = len(pin)
     if strLength > 4:
         print("Invalid PIN length. Correct format is <9876>")
     try:
         pinVal = int(pin)
     except ValueError:
         print("Invalid Pin Character. Correct format is <9876>")
-    if pin == 1234:
+    if pin == "1234":
         print("Your PIN is correct.")
 
 
