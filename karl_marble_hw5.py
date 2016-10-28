1e6e9352023cc2ba9ae3a593ab4a70c802e326d0
Karl <karlmarble@mail.weber.edu>
2016-10-28 19:21:00 +0000

    Added character check and correct pin check.

--- karl_marble_hw5.py
+++ karl_marble_hw5.py
@@ -4,12 +4,21 @@ import sys
 def valid(pin):
     """
     Takes a 4 digit input and checks it against a valid code.
+    Args:
+        pin - a 4 digit passcode
     """
     #1) Check the length
     #2) Check the type
     #3) Check against the code
-    len(valid) = strLength()
-    
+    len(pin) = strLength
+    if strLength > 4:
+        print("Invalid PIN length. Correct format is <9876>")
+    try:
+        pinVal = int(pin)
+    except ValueError:
+        print("Invalid Pin Character. Correct format is <9876>")
+    if pin == 1234:
+        print("Your PIN is correct.")
 
 
 def main():
